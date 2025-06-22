263c59ba1190b1076e0d8c20f23b3975a374dd69
blackfyre <seanrp504@gmail.com>
2025-06-22 17:11:27 -0400

    feat: range, dewey, lcc

--- openLibrary/models/search.py
+++ openLibrary/models/search.py
@@ -1,9 +1,17 @@
-from pydantic import BaseModel, model_validator, field_validator
-from openLibrary.models.books import DeweyDecimal
+from pydantic import (
+    BaseModel,
+    model_validator,
+    field_validator,
+    computed_field,
+    PastDate,
+    FutureDate
+)
+from openLibrary.models.books import ISBN13
 from pydantic_extra_types.language_code import LanguageAlpha2
 from typing import Optional, List
 from openLibrary.common.exceptions import OLValidationError
 from collections.abc import Iterable
+import re
 
 OL_SORT = {
     'editions',
@@ -41,52 +49,104 @@ OL_SORT = {
     'random.daily'
 }
 
-class OLQuery(BaseModel):
-    title: Optional[str | List[str]] = None
-    author: Optional[str | List[str]] = None
-    subject: Optional[str | List[str]] = None
-    place: Optional[str | List[str]] = None
-    person: Optional[str | List[str]] = None
-    publisher: Optional[str | List[str]] = None
-    first_publish_year: Optional[str | List[str]] = None
-    ddn: Optional[DeweyDecimal | List[DeweyDecimal]] = None
+DEWEY_SEARCH_PATTERN = re.compile(r'^\d{1,3}\*?$')
 
-    @field_validator("first_publish_year", mode="before")
-    @classmethod
-    def vaildate_publish_year(cls, years):
-        if type(years) == list:
-            if len(years) < 2 or len(years) > 3:
-                raise OLValidationError("p")
-        return years
+class SupportsRange(BaseModel):
+    start: str | PastDate
+    end: str | FutureDate = None
+
+    @computed_field
+    @property
+    def from_start(self):
+        return f"[{self.start} TO *]"
     
+    @computed_field
+    @property
+    def from_end(self):
+        return f"[* TO {self.end}]"
+    
+    @computed_field
+    @property
+    def from_start_to_end(self):
+        return f"[{self.start} TO {self.end}]"
+
+class DDC(BaseModel):
+    dewey: str | SupportsRange
+
+    @field_validator('dewey')
+    @classmethod
+    def is_dewey(cls, d):
+        if not re.fullmatch(DEWEY_SEARCH_PATTERN, d):
+            raise OLValidationError("Not a valid Dewey Search")
+        return d
+    
+
+class LCC(BaseModel):
+    None
+
+class OLQuery(BaseModel):
+    title: Optional[str] = None
+    subtitle: Optional[str] = None
+    authors: Optional[list[str]] = None
+    subject: Optional[list[str]] = None
+    place: Optional[list[str]] = None
+    person: Optional[list[str]] = None
+    publisher: Optional[list[str]] = None
+    first_publish_year: Optional[SupportsRange] 
+    ddc: Optional[DDC] = None
+    isbn: Optional[ISBN13] = None
+    lcc: Optional[str] = None
+
     @model_validator(mode="after")
     def validate_min_one(self):
-        if not any(getattr(self, field) is not None for field in self.model_fields_set):
-            raise ValueError('At least one value must be present to create a bookID object')
+        if not any(f for f, _ in self.model_dump(exclude_unset=True)):
+            raise ValueError('At least one value must be present to create a Query object')
         return self
     
-    def to_solr(self) -> str:
+    @computed_field
+    @property
+    def time_range(self):
+        key = "first_publish_year"
+        r = {}
+        if self.publish_start:
+            r.update({key: f"[{self.publish_start} TO *]"})
+    
+        if self.publish_start:
+            r.update({key: f"[* TO {self.publish_end}]"})
+        
+        if self.publish_start and self.publish_end:
+            r.update({key: f"[{self.publish_start} TO {self.publish_end}]"})
+        
+        return r
+
+    @computed_field
+    @property
+    def solr(self) -> str:
         ''' conver the model to a solr query'''
         query = []
 
-        for field_name in self.model_fields_set:
-            attr: str = getattr(self, field_name)
+        fields = self.model_dump(exclude_unset=True)
 
-            if field_name == 'first_publish_year' and isinstance(attr, Iterable):
+        for f, v in fields:
+            
+
+            if f.startswith("publish_"):
                 pass
 
-            elif isinstance(attr, Iterable):
+            elif isinstance(v, Iterable):
                 field_values = []
-                for i in attr:
-                    field_values.append(f'{field_name}:"{i}"')
+                for i in v:
+                    field_values.append(f'{f}:"{i}"')
                 
                 ored = str.join(" OR ", field_values)
                 query.append(f"({ored})")
 
             else:
-                query.append(f'{field_name}:"{i}"')
+                query.append(f'{f}:"{i}"')
         
-        return str.join(" AND ", query)
+        query.append(self.time_range) if self.time_range else None
+        
+        return " AND ".join(query)
 
 
 
@@ -94,8 +154,8 @@ class OLQuery(BaseModel):
 class OLSearch(BaseModel):
     """ Model for an Open Library search """
     q: OLQuery
-    fields: Optional[str | List[str]]
-    sort: Optional[str] = None
+    fields: Optional[List[str]]
+    sort: Optional[str] = "title"
     lang: Optional[LanguageAlpha2] = 'en'
 
     @field_validator()
